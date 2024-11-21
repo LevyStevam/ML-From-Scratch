47ea0c5e095f3d407895340d1f862213405a405a
levy <stevamlevy@gmail.com>
2024-11-21 19:16:06 -0300

    normalizer min max

--- models/normalizer.py
+++ models/normalizer.py
@@ -1,24 +1,27 @@
 import numpy as np
 
 class Normalizer:
-    def __init__(self):
-        self.mean = None
-        self.std = None
+    def __init__(self, feature_range=(0, 1)):
+        self.min = None
+        self.max = None
+        self.feature_range = feature_range
 
     def fit(self, data):
-        self.mean = np.mean(data, axis=0)
-        self.std = np.std(data, axis=0)
+        self.min = np.min(data, axis=0)
+        self.max = np.max(data, axis=0)
 
     def transform(self, data):
-        if self.mean is None or self.std is None:
+        if self.min is None or self.max is None:
             raise ValueError("O normalizador precisa ser ajustado com 'fit' antes de transformar os dados.")
-        return (data - self.mean) / self.std
+        scale = self.feature_range[1] - self.feature_range[0]
+        return self.feature_range[0] + (data - self.min) * scale / (self.max - self.min)
 
     def fit_transform(self, data):
         self.fit(data)
         return self.transform(data)
 
     def inverse_transform(self, data_normalized):
-        if self.mean is None or self.std is None:
+        if self.min is None or self.max is None:
             raise ValueError("O normalizador precisa ser ajustado com 'fit' antes de desnormalizar os dados.")
-        return (data_normalized * self.std) + self.mean
+        scale = self.feature_range[1] - self.feature_range[0]
+        return self.min + (data_normalized - self.feature_range[0]) * (self.max - self.min) / scale
